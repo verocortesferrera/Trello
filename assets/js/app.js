d13c189093a5db292ea05d94dd3bc0a9cb7d6576
verocortesferrera <verocortesferrera@gmail.com>
2017-11-10 20:00:10 -0300

    version 0.0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,11 +17,59 @@ var closeList = document.getElementById("close");
 
 
 //creo una funcion que haga que en ese boton al hacer clic ocurra que elimine
-closeList.addEventListener("click", function(){
 
-	//agarro el boton close y le agrego mi lista inicial hidden
-	divBotones.classList.add("hidden");
-	//agarro el contenedor y remuevo  el estilo dado after
-	closeList.classList.remove("after");
+
+var save = document.getElementById('save');
+
+save.addEventListener("click", function(){
+	//llamo a mi input
+	 var lista = document.getElementById('lista').value;
+
+	 //limpio mi lista
+	 document.getElementById('lista').value = "";
+
+	 //llamo al contenedor padre donde ira mi lista
+	 var contenedor = document.getElementById('contenedor');
+
+	 //creo div donde ira la lista
+	 var newContenedorLista = document.createElement('div');
+
+	 //creo elemento p para meter la lista
+	var newLista = document.createElement('p');
+
+	//si el input esta vacio no dejar hacer la lista
+	if(lista.length == 0 || lista == null) { // si no hay nada
+			//desabilita el boton
+		save.disabled = true;// para que se corte ahi la condicion sino sera true y se crea la condicion
+	} 
+	else{}
+
+	//nodo de texto lista
+	var textoListaElement = document.createTextNode(lista);
+	//agrego nodovalorinput a su padre p
+	newLista.appendChild(textoListaElement);
+	//agrego p a su contenedor div creado
+	newContenedorLista.appendChild(newLista);
+	//agrego el div al padre contenedor
+
+	//LLAMO AL elemento que quiero poner despues  en una var
+	 var previo = document.getElementById('lista');
+	 //inserto nuevo contenedor antes del input
+	contenedor.insertBefore(newContenedorLista, previo);
+
+	//darle estilo al contenedor
+	newContenedorLista.classList.add('listaEstilo');
+
 	
+	//añadir un texto añadir una tarea
+	 var nuevaTarea = document.createElement('p');
+	 nuevaTarea.innerHTML = "Añadir nueva tarea";
+	 //lo agrego en el div
+	 contenedor.insertBefore(nuevaTarea, previo);
+
+	
+	 
+
+
+
 });
